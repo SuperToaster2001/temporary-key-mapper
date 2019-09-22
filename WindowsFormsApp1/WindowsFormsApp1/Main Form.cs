eab65338319c4ce74132109fb10dbc4a18729261
SuperToaster2001 <36634189+SuperToaster2001@users.noreply.github.com>
2019-09-23 00:25:48 +0100

    Created Project
    Created the VS project but didn't bother to change the name of the VS project

---
+++ WindowsFormsApp1/WindowsFormsApp1/Main Form.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ScriptsLib;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            New_Profile newMDIChild = new New_Profile();
+            newMDIChild.MdiParent = this;
+            newMDIChild.Show();
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/New Profile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class New_Profile : Form
+    {
+        public New_Profile()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void New_Profile_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
